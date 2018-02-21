87e9405eb9605aacd7700f868aa36d9e56c646ad
Louis <Louis@192.168.0.31>
2018-02-21 21:15:37 +0000

    initial commit

---
+++ hope/first.py
@@ -0,0 +1,18 @@
+'''
+Created on 21 Feb 2018
+
+@author: Louis
+'''
+import platform
+
+def cpu():
+    print(platform.processor())
+    return
+
+def os():
+    print(platform.platform())
+    return
+
+if __name__ == '__main__':
+    pass
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="hope",
+    version="0.1",
+    description="Basic system info",
+    url="",
+    author="louisgh",
+    author_email="louisgateshardiman@gmail.com",
+    licence="GPL3",
+    packages=['hope'],
+  
+)
